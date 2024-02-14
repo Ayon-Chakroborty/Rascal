7297318bcef86d7ac027e67c8c83f7ac1c5fad18
Ayon-Chakroborty <gv5072@wayne.edu>
2024-02-14 11:47:33 -0500

    Now you can disassemble the bytecode and see which chunks were created and what OpCode they contain. Helpfull for debugging

---
+++ src/debug.h
@@ -0,0 +1,9 @@
+#ifndef rascal_debug_h
+#define rascal_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
